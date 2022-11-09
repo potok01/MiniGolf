d88d115f7ea7e1e00b6490be786e40c85b30baf6
Jason Landis <jasonelandis@gmail.com>
2022-11-09 15:08:27 -0500

    Finished golf ball controller

--- Assets/Scripts/GolfBallController.cs
+++ Assets/Scripts/GolfBallController.cs
@@ -2,38 +2,49 @@ using UnityEngine;
 
 public class GolfBallController : MonoBehaviour
 {
-    public float maxSpeed = 10.0f;
-    public float angle = 0.0f;
-    public float velocity = 0.0f;
+    public float angle = 90.0f;
+    float maxAngle = 180.0f;
+    float minAngle = 0.0f;
+
+    public float power = 100.0f;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            // change the projectory of the ball to the left
-        }
-
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            // change the projectory of the ball to the right            
+            // change the angle of the ball to the left
+            if (angle > minAngle)
+            {
+                angle -= 10.0f;
+            } 
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // change the angle of the ball to the right
+            if (angle < maxAngle)
+            {
+                angle += 10.0f;
+            }            
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            // increase the power to be applied to the ball          
+            // increase the power to be applied to the ball
+            power += 100.0f;            
         }
-
+        
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            // decrease the power to be applied to the ball            
+            // decrease the power to be applied to the ball
+            power -= 100.0f;           
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            angle = Random.Range(0.0f, 360.0f);
-            velocity = Random.Range(0.0f, maxSpeed);
-            Vector3 force = new Vector3(Mathf.Cos(angle), 0.0f, Mathf.Sin(angle)) * velocity;
-            GetComponent<Rigidbody2D>().AddForce(force);
+            // apply a force to the ball in the direction of the angle and power using vector2d
+            Vector2 direction = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+            GetComponent<Rigidbody2D>().AddForce(direction * power);
         }
     }
 }
